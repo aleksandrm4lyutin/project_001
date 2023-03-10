42677eab4af7cbbbd41ae8fe996129597806c351
Alexander <4braKaqa6r4@gmail.com>
2023-03-10 14:30:55 +0300

    initial commit 6

--- lib/pages/web_view_page.dart
+++ lib/pages/web_view_page.dart
@@ -15,66 +15,108 @@ class WebViewPage extends StatefulWidget {
 
 class _WebViewPageState extends State<WebViewPage> {
 
-  final WebViewController webViewController = WebViewController();
-  bool isLoading = true;
+  //final WebViewController webViewController = WebViewController();
+  WebViewController? _controller;
+
+  late bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
     //isLoading = true;
-    webViewController
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0xFF212121))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            setState(() {
-              isLoading = true;
-            });
-          },
-          onPageStarted: (String url) {
-          },
-          onPageFinished: (String url) {
-            setState(() {
-              isLoading = false;
-            });
-          },
-          onWebResourceError: (WebResourceError error) {
-          },
-          onNavigationRequest: (NavigationRequest request) {
-            debugPrint('allowing navigation to ${request.url}');
-            return NavigationDecision.navigate;
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse(widget.link));
+    // webViewController
+    //   ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    //   ..setBackgroundColor(const Color(0xFF212121))
+    //   ..setNavigationDelegate(
+    //     NavigationDelegate(
+    //       onProgress: (int progress) {
+    //         setState(() {
+    //           isLoading = true;
+    //         });
+    //       },
+    //       onPageStarted: (String url) {
+    //       },
+    //       onPageFinished: (String url) {
+    //         setState(() {
+    //           isLoading = false;
+    //         });
+    //       },
+    //       onWebResourceError: (WebResourceError error) {
+    //       },
+    //       onNavigationRequest: (NavigationRequest request) {
+    //         debugPrint('allowing navigation to ${request.url}');
+    //         return NavigationDecision.navigate;
+    //       },
+    //     ),
+    //   )
+    //   ..loadRequest(Uri.parse(widget.link));
   }
 
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        var b = await webViewController.canGoBack();
-        if(b) {
-          webViewController.goBack();
-        }
-        return false;
-      },
-      child: SafeArea(
-        child: Stack(
-          children: [
-            WebViewWidget(
-              controller: webViewController,
-            ),
-            isLoading ? const Center(
-                child: CircularProgressIndicator(
-                  color: Colors.white,
+
+    return SafeArea(
+      child: Builder(
+        builder: (BuildContext context) {
+          return WillPopScope(
+            onWillPop: () async {
+              if(await _controller!.canGoBack()) {
+                _controller!.goBack();
+              }
+              return false;
+            },
+            child: Stack(
+              children: [
+                WebView(
+                  onProgress: (int progress) {
+                    setState(() {
+                      isLoading = true;
+                    });
+                  },
+                  onPageFinished: (String url) {
+                    setState(() {
+                      isLoading = false;
+                    });
+                  },
+                  initialUrl: widget.link,
+                  javascriptMode: JavascriptMode.unrestricted,
+                  onWebViewCreated: (WebViewController webViewController) {
+                    _controller = webViewController;
+                  },
+                  gestureNavigationEnabled: true,
                 ),
-            ) : const SizedBox(height: 0,),
-          ],
-        )
+                isLoading ? const Center(
+                  child: CircularProgressIndicator(color: Colors.white,),
+                ) : const SizedBox(height: 0,),
+              ],
+            ),
+          );
+        },
       ),
     );
+    // return WillPopScope(
+    //   onWillPop: () async {
+    //     var b = await webViewController.canGoBack();
+    //     if(b) {
+    //       webViewController.goBack();
+    //     }
+    //     return false;
+    //   },
+    //   child: SafeArea(
+    //     child: Stack(
+    //       children: [
+    //         WebViewWidget(
+    //           controller: webViewController,
+    //         ),
+    //         isLoading ? const Center(
+    //             child: CircularProgressIndicator(
+    //               color: Colors.white,
+    //             ),
+    //         ) : const SizedBox(height: 0,),
+    //       ],
+    //     )
+    //   ),
+    // );
   }
 }
